fe4314e3e34b015cd552b41da40c5bd516a9a3b9
Jeirsen <jeirsen@gmail.com>
2019-10-15 22:49:38 -0500

    pushed simple vuex project

---
+++ src/views/Cats.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <PetTable species="cats" :pets="cats" />
+  </div>
+</template>
+
+<script>
+import PetTable from "@/components/PetTable.vue";
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    PetTable
+  },
+  computed: {
+    ...mapState(["cats"])
+  }
+};
+</script>
